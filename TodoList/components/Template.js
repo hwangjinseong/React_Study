41872810d7aefa13e235646b3af74343120f6636
hwangjinseong <imhjs2023@gmail.com>
2022-05-12 20:23:59 +0900

    ADD

--- TodoList/components/Template.js
+++ TodoList/components/Template.js
@@ -3,22 +3,32 @@ import styled from 'styled-components';
 
 const Temple = styled.div`
 padding-top: 20px;
-max-height: 100vh;
+  max-height: 100vh;
 `;
 
 const Title = styled.div`
-width: 400px;
-margin-top: 30px;
-padding-bottom: 20px;
-font-size: 1.5rem;
-font-weight: bold;
-color: #6c567b;
+display: flex;
+justify-content: center;
+font-size: 60px;
+color: #8c82FF;
+`;
+
+const Title2 = styled.div`
+  width: 400px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 20px;
+  padding-bottom: 20px;
+  font-size: 1.3rem;
+  font-weight: bold;
+  color: #6c567b;
 `;
 
 function Template({children, todoLength}) {
   return (
     <Temple>
-      <Title>오늘의 할 일 ({todoLength})</Title>
+      <Title>TodoList</Title>
+      <Title2>할 일 ({todoLength})</Title2>
       <div>{children}</div>
     </Temple>
   )
